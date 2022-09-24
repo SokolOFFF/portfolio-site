1806ebebce9bce4692221f9a8b9eec7fb696ed3f
SokolOFFF <egor1sokolov@gmail.com>
2022-09-24 15:52:57 +0300

    added views to app

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from portfolio_app.models import Project, Contact
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'text': "Hello, this is my personal page! Here you can find everything you want about me."
+    }
+    return render(request=request, template_name='converter_app/index.html', context=context)
+
+
+def projects(request):
+    all_projects = Project.objects.all()
+    context = {
+        'projects': all_projects
+    }
+    return render(request=request, template_name='converter_app/projects.html', context=context)
+
+
+def contacts(request):
+    return render(request=request, template_name='converter_app/contacts.html')
