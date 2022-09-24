e37e99516001a3abb3a99327892903b1bbfd8a74
SokolOFFF <egor1sokolov@gmail.com>
2022-09-24 15:52:40 +0300

    created two models: contact and project

--- portfolio/portfolio_app/models.py
+++ portfolio/portfolio_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    link = models.TextField(default='https://github.com/SokolOFFF')
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=10)
+    link = models.TextField(default='https://vk.com/yeawou')
